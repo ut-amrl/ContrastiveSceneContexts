d3d7f50395026a9ec748da333d5567d854fa52ae
Amanda Adkins <mandi1267@gmail.com>
2021-11-24 19:17:37 -0600

    Add script just for printing model and weights

---
+++ cluster_representation_learning/model_printer.py
@@ -0,0 +1,37 @@
+
+from model.cluster_label_model import ClusterLabelModel
+import torch
+from omegaconf import OmegaConf
+import hydra
+import os
+import logging
+
+import lib.unoriginal.multiprocessing_utils as mpu
+
+torch.manual_seed(0)
+torch.cuda.manual_seed(0)
+
+@hydra.main(config_path='config', config_name='defaults.yaml')
+def main(config):
+  if os.path.exists('config.yaml'):
+    logging.info('===> Loading exsiting config file')
+    config = OmegaConf.load('config.yaml')
+    logging.info('===> Loaded exsiting config file')
+  logging.info('===> Configurations')
+
+  if config.misc.num_gpus > 1:
+      mpu.multi_proc_run(config.misc.num_gpus,
+              fun=single_proc_run, fun_args=(config,))
+  else:
+      single_proc_run(config)
+
+def single_proc_run(config):
+  num_feats=1 # LiDAR just has reflectance/intensity
+  model = ClusterLabelModel(num_feats, config.net.model_n_out, config, D=3)
+  model = model.double()
+  print(model)
+  model.updateWithPretrainedWeights(config.net.weights)
+
+if __name__ == "__main__":
+  os.environ['MKL_THREADING_LAYER'] = 'GNU'
+  main()
---
+++ cluster_representation_learning/scripts/print_model.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python model_printer.py  \
+    net.conv1_kernel_size=3 \
+    net.model_n_out=64 \
+    misc.num_gpus=1 \
+    misc.out_dir=${OUT_DIR} \
+    net.weights=$PRETRAIN \
